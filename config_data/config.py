b8a3e0f5bb342d652284d551734882c0f8e7ffa6
Stas Rudy <stasrudy@gmail.com>
2023-04-24 15:19:21 +0300

    Basic commands in main.ru, env file.

--- config_data/config.py
+++ config_data/config.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-#if not find_dotenv():
-#    exit("Переменные окружения не загружены т.к отсутствует файл .env")
-#else:
-#    load_dotenv()
+if not find_dotenv():
+    exit("Переменные окружения не загружены т.к отсутствует файл .env")
+else:
+    load_dotenv()
 
-BOT_TOKEN = os.getenv("5935970959:AAECy_YcIq5Psi8RsiVypXQvtO4NlFU0Lys")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
-    ("help", "Вывести справку")
+    ("help", "Вывести справку"),
+    ("echo", "Повторить сообщение")
 )
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,36 +1,8 @@
 from telebot.types import BotCommand
 from config_data.config import DEFAULT_COMMANDS
-import telebot
 
 
 def set_default_commands(bot):
     bot.set_my_commands(
         [BotCommand(*i) for i in DEFAULT_COMMANDS]
     )
-
-
-# Создаем объект бота
-bot = telebot.TeleBot("5935970959:AAECy_YcIq5Psi8RsiVypXQvtO4NlFU0Lys")
-
-# Обработка команды /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот. Чем могу помочь?")
-
-# Обработка команды /help
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, "Это список доступных команд:\n/start - начать работу с ботом\n/help - получить справку\n/echo - эхо-ответ на ваше сообщение")
-
-# Обработка команды /echo
-@bot.message_handler(commands=['echo'])
-def send_echo(message):
-    bot.reply_to(message, message.text)
-
-# Обработка текстовых сообщений
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-# Запускаем бота
-bot.polling()
