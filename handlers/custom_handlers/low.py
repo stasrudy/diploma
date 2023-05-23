c16213b96e3b09c6f2af4f386284136573369d4e
Stas Rudy <stasrudy@gmail.com>
2023-05-23 15:29:21 +0300

    Trying command low with API - find city

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,14 +1,37 @@
 from telebot.types import Message
 from loader import bot
+from config_data.config import RAPID_API_KEY
 import requests
 import json
-
+import pprint
 
 @bot.message_handler(commands=["low"])
 def bot_low(message: Message):
-    my_request = requests.get('https://rapidapi.com/apidojo/api/hotels4/properties/v2/list')
-    data = json.loads(my_request.text)
-    print(data)
+    location = input('В каком городе ищем отель?')
+    url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+    querystring = {"query": location, "locale": "ru_RU", "currency": "USD"}
+
+    headers = {
+        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    pprint.pprint(response.json())
 
 
+#DEFAULT_COMMANDS = (
+#    ("start", "Запустить бота"),
+#    ("help", "Вывести справку"),
+#    ("echo", "Повторить сообщение"),
+#    ('low', 'Поиск низких цен')
+#)
+
+#bot.set_my_commands(
+#        [BotCommand(*i) for i in DEFAULT_COMMANDS]
+
+
+#спросить какой город
+#дать выбрать варианты городов вытянутые из API - обращение к API
+#получить по выбранному варианту ID локации
+#по этому ID найти с самой низкой ценой hotel - обращение к API
 
